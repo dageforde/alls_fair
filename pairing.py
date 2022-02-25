a375620de4637d91ca4af31439c000b9fb0dfb60
Christiaan Dageforde <cdag527@gmail.com>
2022-02-25 15:30:43 -0600

    rough draft complete

--- pairing.py
+++ pairing.py
@@ -3,122 +3,76 @@ from itertools import combinations
 from datetime import datetime, timedelta
 
 # load project json
-f = open('project.json')#sys input?
+f = open('project_copy.json')
 project_data = json.load(f)
 # for i in project_data['project']:
 #     print(i)
 
 # set variables from json
-devs = project_data['project']['dev_names'] # list of developers
-nsprints = project_data['project']['num_sprints'] # number of sprints
-dev_pairs = [",".join(map(str, combo)) for combo in combinations(devs, 2)] # list of possible 1:1 pairing combinations; 5 devs should yield 10 pairs
-project_start_date = (datetime.strptime(project_data['project']['start_date'], "%B %d, %Y")) # convert date string to datetime
-sprint_days = project_data['project']['sprint_days'] # number of days in sprint
-# print(dev_pairs)
-# print(len(dev_pairs)) 
-
+devs = project_data['project']['dev_names'] 
+nsprints = project_data['project']['num_sprints'] 
+project_start_date = project_data['project']['start_date']
+sprint_days = project_data['project']['sprint_days']  
+# dev_pairs = [",".join(map(str, combo)) for combo in combinations(devs, 2)] # list of possible 1:1 pairing combinations; 
+# print(len(dev_pairs)) # 5 devs should yield 10 pairs
+print(project_data['project'])
 
 # function to create team rotations
-rotations=list()
 def Rotate(l1,num):
     for i in range(0,num):
         l1 = [l1[(i + 4) % len(l1)] for i, x in enumerate(l1)]
         rotations.append(l1)
         l1=rotations[-1]
-# rotations=[] # empty list to store team rotations
+rotations=list() # empty list to store team rotations
 Rotate(devs,nsprints)
-# print(rotations[0][0:2])
+
 
 
 # function to assign pairs, solo devs per sprint
+
 sprint_assigns = dict() # dict of pairs & solo for each sprint
-all_sprints = list()
-sprint_num = 1
-sprint_start = project_start_date
+all_sprints = list() # list to store all sprint rotation schedule info
+sprint_num = 1 # begin numbering sprints at 1
+sprint_start_obj = datetime.strptime(project_start_date, "%B %d, %Y") # convert to datetime object for timedelta 
 for r in rotations: #for every rotated team
     assn_list = list() # list to store the pair assignments
-    chunk_size = 2 # chunk the list by elements of 2
+    chunk_size = 2 
     pair_count = 1
     sprint_title = 'Sprint_'+str(sprint_num)
     for i in range(0, len(r), chunk_size): # iterating by 2 over a range between 0 and team size
         pair_n = 'Pair_'+str(pair_count)
-        pair_assn = r[i:i+chunk_size]
-        if len(pair_assn) == 1:
-            pair_dict = {'Solo':pair_assn}
+        pair_assn = r[i:i+chunk_size] #chunk team by n chunk_size; should
+        if len(pair_assn) == 1: # if only one dev assigned, they are Solo
+            pair_dict = {'Solo':pair_assn} 
         else:
             pair_dict = {pair_n:pair_assn}
         assn_list.append(pair_dict)
         pair_assn_dict = {'pairs':assn_list}
         sprint={sprint_title : pair_assn_dict,
-                'start_date' : sprint_start,
+                'start_date' : datetime.strftime(sprint_start_obj,"%B %d %Y"),
                 'days':sprint_days}
         pair_count+=1 
-    # sprint_pairs.append(assn_list)
-    # sprint_assigns.update(sprint_title)
     all_sprints.append(sprint)
     sprint_num+=1
-    sprint_start = project_start_date + timedelta(days=7)
-# sprint_dict = {'project'+n:sprint_pairs}
+    sprint_start_obj = sprint_start_obj + timedelta(days=7)
 project_dict = {'project':all_sprints}
 # print(project_dict)
-json_object = json.dumps(project_dict, indent = 4) 
-print(json_object)
 
 
 
+# import json
+# s = json.dumps(project_dict)
+# d = json.loads(s)
+# print(d)
+# # json_object = json.dumps(project_dict, indent = 4) 
+# print(json_object)
 
 
-
-
-
-
-
-
-
-
-# new_date = current_date + timedelta(weeks=4)
-
-# res = project_start_date + timedelta(days=7)
-# print(project_start_date)
-# print(res)
-
-# print(chunked_list[0])
-# print(sprint_pairs)
-# for i in sprint_pairs:
-#     print(i , sprint_pairs.count(i))
-
-
-# pair_nest = {Project DICT
-#               Sprints LIST :
-#               'Sprint' DICT:
-#                 {'pairs DICT':
-#                     ['Pair_1':[a,b],'Pair_2':[c,d],'Solo':[e]]},
-#                  'start_date':yyyy-mm-dd}}
-
-
-# dict_keys = ['Pair_1','Pair_2','Solo']
-# sprint_name = []
-# paired_sprints = []#dict of sprints with pair assignments
-# n=1
-# for pair in sprint_pairs:
-#     sprint_n = 'Sprint_'+str(n)
-#     # pairs_dict = dict(zip(dict_keys, pair))
-#     pairs_dict = dict.fromkeys(dict_keys, pair)
-#     sprint_name.append(sprint_n)
-#     sprint_n_dict = dict.fromkeys(sprint_name,pairs_dict) # dict of pairing assignments per sprint
-#     # paired_sprints.append(sprint_n_dict)
-#     n+=1
-#     # print(pair)
-#     # print(sprint_n)
-# # print(sprint_pairs[0])
-# # print('sprnt n dict: ',sprint_n_dict)
-
-
-
-
-
-#figure most even distribution of pairs DONE-ISH
-# assign dates to each sprint
 #write to calendar 
 #write to data store
 #function to enter date and see pairs that day
+# print(type(project_dict['project']))
+# def printSchedule():
+#     for sprint in project_dict['project']:
+
+    # print(project_dict['project'][0])
